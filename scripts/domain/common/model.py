df35dab369e7b557c91d7436a9d6cc03e96924b7
Toshiya Nakakura <nakakura@me.com>
2020-07-02 16:06:01 +0900

    define Value objects of ids

--- scripts/domain/common/model.py
+++ scripts/domain/common/model.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from error import MyException
 
-
+# ---------- Peer ----------
 class PeerInfo:
     """
     Information for identifying PeerObject
@@ -45,6 +45,169 @@ class PeerInfo:
         if not isinstance(other, PeerInfo):
             return NotImplemented
         return self.__peer_id == other.__peer_id and self.__token == other.__token
+
+    def __ne__(self, other):
+        return not self.__eq__(other)
+
+
+# ---------- Data ----------
+class DataId:
+    def __init__(self, data_id):
+        """
+        a Value object of data_id
+        :param unicode data_id: ID to identify the socket that will receive the data from the end user program
+        """
+
+        if isinstance(data_id, str):
+            data_id = data_id.decode("utf-8")
+        # TOKEN is prefix(da-, 3words) + UUID(36words) = 39words
+        if (
+            not isinstance(data_id, unicode)
+            or len(data_id) != 39
+            or not data_id.startswith("da-")
+        ):
+            raise MyException("invalid data_id")
+        self.__data_id = data_id
+
+    def id(self):
+        # type: () -> unicode
+        return self.__data_id
+
+    def __eq__(self, other):
+        if not isinstance(other, DataId):
+            return NotImplemented
+
+        return self.id() == other.id()
+
+    def __ne__(self, other):
+        return not self.__eq__(other)
+
+
+class DataConnectionId:
+    def __init__(self, data_connection_id):
+        """
+        a Value object of data_connection_id
+        :param unicode data_connection_id: ID to identify the DataConnection
+        """
+
+        if isinstance(data_connection_id, str):
+            data_connection_id = data_connection_id.decode("utf-8")
+
+        # TOKEN is prefix(dc-, 3words) + UUID(36words) = 39word                                   s
+        if (
+            not isinstance(data_connection_id, unicode)
+            or len(data_connection_id) != 39
+            or not data_connection_id.startswith("dc-")
+        ):
+            raise MyException("invalid media_connection_id")
+        self.__data_connection_id = data_connection_id
+
+    def id(self):
+        # type: () -> unicode
+        return self.__data_connection_id
+
+    def __eq__(self, other):
+        if not isinstance(other, DataConnectionId):
+            return NotImplemented
+
+        return self.id() == other.id()
+
+    def __ne__(self, other):
+        return not self.__eq__(other)
+
+
+# ---------- Media ----------
+class MediaId:
+    def __init__(self, media_id):
+        """
+        a Value object of media_id
+        :param unicode media_id: ID to identify the socket that will receive the media from the end user program
+        """
+
+        if isinstance(media_id, str):
+            media_id = media_id.decode("utf-8")
+        # TOKEN is prefix(da-, 3words) + UUID(36words) = 39words
+        if (
+            not isinstance(media_id, unicode)
+            or len(media_id) != 39
+            or (not media_id.startswith("vi-") and not media_id.startswith("au-"))
+        ):
+            raise MyException("invalid media_id")
+        self.__media_id = media_id
+
+    def id(self):
+        # type: () -> unicode
+        return self.__media_id
+
+    def __eq__(self, other):
+        if not isinstance(other, MediaId):
+            return NotImplemented
+
+        return self.id() == other.id()
+
+    def __ne__(self, other):
+        return not self.__eq__(other)
+
+
+class RtcpId:
+    def __init__(self, rtcp_id):
+        """
+        a Value object of rtcp_id
+        :param unicode rtcp_id: ID to identify the socket that will receive the rtcp from the end user program
+        """
+
+        if isinstance(rtcp_id, str):
+            rtcp_id = rtcp_id.decode("utf-8")
+        # TOKEN is prefix(da-, 3words) + UUID(36words) = 39words
+        if (
+            not isinstance(rtcp_id, unicode)
+            or len(rtcp_id) != 39
+            or not rtcp_id.startswith("rc-")
+        ):
+            raise MyException("invalid rtcp_id")
+        self.__rtcp_id = rtcp_id
+
+    def id(self):
+        # type: () -> unicode
+        return self.__rtcp_id
+
+    def __eq__(self, other):
+        if not isinstance(other, RtcpId):
+            return NotImplemented
+
+        return self.id() == other.id()
+
+    def __ne__(self, other):
+        return not self.__eq__(other)
+
+
+class MediaConnectionId:
+    def __init__(self, media_connection_id):
+        """
+        a Value object of rtcp_id
+        :param unicode media_connection_id: ID to identify the MediaConnection
+        """
+
+        if isinstance(media_connection_id, str):
+            media_connection_id = media_connection_id.decode("utf-8")
+        # TOKEN is prefix(da-, 3words) + UUID(36words) = 39words
+        if (
+            not isinstance(media_connection_id, unicode)
+            or len(media_connection_id) != 39
+            or not media_connection_id.startswith("mc-")
+        ):
+            raise MyException("invalid media_connection_id")
+        self.__media_connection_id = media_connection_id
+
+    def id(self):
+        # type: () -> unicode
+        return self.__media_connection_id
+
+    def __eq__(self, other):
+        if not isinstance(other, MediaConnectionId):
+            return NotImplemented
+
+        return self.id() == other.id()
 
     def __ne__(self, other):
         return not self.__eq__(other)
--- test/domain/common/model/test_model.py
+++ test/domain/common/model/test_model.py
@@ -11,7 +11,14 @@ sys.path.insert(
     + "/scripts",
 )
 from error import MyException
-from domain.common.model import PeerInfo
+from domain.common.model import (
+    PeerInfo,
+    DataId,
+    DataConnectionId,
+    MediaId,
+    RtcpId,
+    MediaConnectionId,
+)
 
 
 class TestPeerInfo:
@@ -54,3 +61,162 @@ class TestPeerInfo:
     def test_create_peer_info_failed(self, peer_id, token):
         with pytest.raises(MyException):
             _peer_info = PeerInfo(peer_id, token)
+
+
+# ----------Data ----------
+class TestDataId:
+    def setup_method(self, method):
+        self.data_id = DataId(u"da-50a32bab-b3d9-4913-8e20-f79c90a6a211")
+
+    def teardown_method(self, method):
+        del self.data_id
+
+    def test_compare_same_id(self):
+        assert self.data_id == DataId("da-50a32bab-b3d9-4913-8e20-f79c90a6a211")
+
+    def test_compare_different_id(self):
+        assert self.data_id != DataId(u"da-14a32bab-b3d9-4913-8e20-f79c90a6a211")
+
+    @pytest.mark.parametrize(
+        "data_id",
+        [
+            # short
+            u"da-50a32bab-b3d9-4913-8e20-f79c90a6a21",
+            # long
+            u"da-50a32bab-b3d9-4913-8e20-f79c90a6a2112",
+            # wrong prefix
+            u"dc-50a32bab-b3d9-4913-8e20-f79c90a6a211",
+        ],
+    )
+    def test_create_fail(self, data_id):
+        with pytest.raises(MyException):
+            _data_id = DataId(data_id)
+
+
+class TestDataConnectionId:
+    def setup_method(self, method):
+        self.data_connection_id = DataConnectionId(
+            u"dc-50a32bab-b3d9-4913-8e20-f79c90a6a211"
+        )
+
+    def teardown_method(self, method):
+        del self.data_connection_id
+
+    def test_compare_same_id(self):
+        assert self.data_connection_id == DataConnectionId(
+            "dc-50a32bab-b3d9-4913-8e20-f79c90a6a211"
+        )
+
+    def test_compare_different_id(self):
+        assert self.data_connection_id != DataConnectionId(
+            u"dc-10032bab-b3d9-4913-8e20-f79c90a6a211"
+        )
+
+    @pytest.mark.parametrize(
+        "data_connection_id",
+        [
+            # short
+            u"dc-50a32bab-b3d9-4913-8e20-f79c90a6a21",
+            # long
+            u"dc-50a32bab-b3d9-4913-8e20-f79c90a6a2112",
+            # wrong prefix
+            u"da-50a32bab-b3d9-4913-8e20-f79c90a6a211",
+        ],
+    )
+    def test_create_fail(self, data_connection_id):
+        with pytest.raises(MyException):
+            _data_connection_id = DataConnectionId(data_connection_id)
+
+
+# ----------Media ----------
+class TestMediaId:
+    def setup_method(self, method):
+        self.media_id = MediaId(u"vi-50a32bab-b3d9-4913-8e20-f79c90a6a211")
+
+    def teardown_method(self, method):
+        del self.media_id
+
+    def test_compare_same_id(self):
+        assert self.media_id == MediaId("vi-50a32bab-b3d9-4913-8e20-f79c90a6a211")
+
+    def test_compare_different_id(self):
+        assert self.media_id != MediaId(u"au-14a32bab-b3d9-4913-8e20-f79c90a6a211")
+
+    @pytest.mark.parametrize(
+        "media_id",
+        [
+            # short
+            u"vi-50a32bab-b3d9-4913-8e20-f79c90a6a21",
+            # long
+            u"au-50a32bab-b3d9-4913-8e20-f79c90a6a2112",
+            # wrong prefix
+            u"dc-50a32bab-b3d9-4913-8e20-f79c90a6a211",
+        ],
+    )
+    def test_create_fail(self, media_id):
+        with pytest.raises(MyException):
+            _media_id = MediaId(media_id)
+
+
+class TestRtcpId:
+    def setup_method(self, method):
+        self.rtcp_id = RtcpId(u"rc-970f2e5d-4da0-43e7-92b6-796678c104ad")
+
+    def teardown_method(self, method):
+        del self.rtcp_id
+
+    def test_compare_same_id(self):
+        assert self.rtcp_id == RtcpId("rc-970f2e5d-4da0-43e7-92b6-796678c104ad")
+
+    def test_compare_different_id(self):
+        assert self.rtcp_id != RtcpId(u"rc-14a32bab-b3d9-4913-8e20-f79c90a6a211")
+
+    @pytest.mark.parametrize(
+        "rtcp_id",
+        [
+            # short
+            u"rc-50a32bab-b3d9-4913-8e20-f79c90a6a21",
+            # long
+            u"rc-50a32bab-b3d9-4913-8e20-f79c90a6a2112",
+            # wrong prefix
+            u"pt-50a32bab-b3d9-4913-8e20-f79c90a6a211",
+        ],
+    )
+    def test_create_fail(self, rtcp_id):
+        with pytest.raises(MyException):
+            _media_id = MediaId(rtcp_id)
+
+
+class TestMediaConnectionId:
+    def setup_method(self, method):
+        self.media_connection_id = MediaConnectionId(
+            u"mc-102127d9-30de-413b-93f7-41a33e39d82b"
+        )
+
+    def teardown_method(self, method):
+        del self.media_connection_id
+
+    def test_compare_same_id(self):
+        assert self.media_connection_id == MediaConnectionId(
+            "mc-102127d9-30de-413b-93f7-41a33e39d82b"
+        )
+
+    def test_compare_different_id(self):
+        assert self.media_connection_id != MediaConnectionId(
+            u"mc-aa2127d9-30de-413b-93f7-41a33e39d82b"
+        )
+
+    @pytest.mark.parametrize(
+        "media_id",
+        [
+            # short
+            u"mc-50a32bab-b3d9-4913-8e20-f79c90a6a21",
+            # long
+            u"mc-50a32bab-b3d9-4913-8e20-f79c90a6a2112",
+            # wrong prefix
+            u"pt-50a32bab-b3d9-4913-8e20-f79c90a6a211",
+        ],
+    )
+    def test_create_fail(self, media_id):
+        with pytest.raises(MyException):
+            _media_id = MediaId(media_id)
